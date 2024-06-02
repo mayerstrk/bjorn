5ebdbce41bcb6152499aab5b6445fbdded0f9b18
Mayer Starkman <mstarkcode@gmail.com>
2024-06-02 10:24:59 +0300

    feat: add csutom cursor and backround

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
+	import { DoubleBounce } from 'svelte-loading-spinners';
 	import '../app.css';
 	import Logo from '../lib/components/logo.svelte';
 	import Controls from '../lib/components/nav-controls.svelte';
+
+	let posX = 0;
+	let posY = 0;
+
+	function updatePosition(event: MouseEvent) {
+		// The circle is 32px in diameter (w-8 h-8 in Tailwind is 2rem or 32px), so we adjust by half of that.
+		posX = event.clientX - 16; // Subtract half the width
+		posY = event.clientY - 16; // Subtract half the height
+	}
 </script>
 
 <page
+	role="main"
 	id="root-layout-page"
-	class="scrollbar-hidden mx-auto flex h-full w-full min-w-[320px] max-w-screen-xl grow flex-col overflow-y-auto px-5 text-stone-100"
+	class="scrollbar-hidden mx-auto flex h-full w-full min-w-[320px] max-w-screen-xl grow cursor-none flex-col overflow-y-auto px-5 text-stone-100"
 	onscroll={() => console.log('scrolling')}
+	on:mousemove={updatePosition}
 >
+	<div
+		class="pointer-events-none absolute"
+		style="top: {posY}px; left: {posX}px;"
+	>
+		<DoubleBounce size="32" duration="4s" color="white" />
+	</div>
 	<nav
 		id="root-layout-nav"
 		class="sticky inset-x-0 top-0 flex w-full items-center justify-between py-5 backdrop-blur-sm"
@@ -25,3 +43,18 @@
 		</p>
 	</footer>
 </page>
+
+<style>
+	/* Tailwind does not natively support keyframes or complex animations, so adding basic CSS for pulsing */
+	@keyframes pulse {
+		0%,
+		100% {
+			transform: scale(1);
+			opacity: 1;
+		}
+		50% {
+			transform: scale(1.5);
+			opacity: 0.5;
+		}
+	}
+</style>
