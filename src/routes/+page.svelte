9cab64edca8cd67e19678407b56785edb4f6a02d
Mayer Starkman <mstarkcode@gmail.com>
2024-08-24 14:24:43 +0300

    refactor: improve experience section markup: description and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 	$effect(() => {});
 </script>
 
-<section id="root-hero">
+<section class="section" id="root-hero">
 	<div
 		id="root-hero-dinmensional-bjorn-wrapper"
 		class="-z-5 h-[30vh] bg-transparent"
@@ -54,39 +54,50 @@
 			/>
 		</div>
 	</div>
-	<p id="root-hero-about-me" class="py-2">
+	<p id="root-hero-about-me" class="m-auto max-w-[900px] py-2">
 		&nbsp;&nbsp;&nbsp;&nbsp;I am passionate about designing and building
 		efficient, <span class="underline underline-offset-4">simple</span>
 		systems that look good and are easy to use - from drawing up process diagrams,
 		UX design and infrastructure provisioning, all the way to running pilots and
 		handling subsequent feature requests and code-base maintenance. Crafting apps
 		that offer an incredible experience for both developers and users and optimizing
-		for performance is something I find truly rewarding.
+		for <span class="underline underline-offset-4">performance</span> is
+		something I find truly rewarding.
 		<span class="opacity-0 transition-opacity hover:opacity-100">
 			I don't want JS on the backend :)</span
 		>
 	</p>
 </section>
-<section>
-	<h3 class="text-lg font-black underline underline-offset-8">Experience</h3>
-	<div class="block-stone-300 relative font-mono font-bold">
-		<span class="code-line-pink text-white">IDF</span>&nbsp; Was given the
-		opportunity to open a department and recruit members to it. Our purpose was
-		digitalizing and creating visualizations(BI) for day-to-day business
-		processes and deploying branch and role specific mobile and web based
-		applications that needed to interface.
+<section class="section">
+	<h3 class="text-lg font-black underline underline-offset-8">
+		&nbsp;Experience&nbsp;
+	</h3>
+	<div class="block-stone-300 relative p-5 md:p-10">
+		<span class="code-line-pink text-white">IDF</span>&nbsp;&nbsp;&nbsp;I was
+		given the opportunity to
+		<span class="font-bold">lead a new department</span>
+		focused on digitizing and visualizing business processes, where I
+		<span class="font-bold">engineered two cross-platform applications</span>
+		that are currently active in production. In addition to providing a digital interface
+		for the branches' business processes the applications would collect activity
+		data and implement automation and data pipelines to support data-driven decision-making
+		through comprehensive dashboards. Eventually, I also recruited, mentored, and
+		managed a dedicated team of developers, maintaining high standards of code quality
+		and performance.
 		{#if showMore}
 			<span class="mt-4 block font-light">
-				I led a team of mobile and web developers in the Israeli Defence Forces.
-				Our work involved understanding the specific processes of our
-				organization and engineering systems that enhance and automate their
-				workflows. We specialized in creating solutions that collect, analyze,
-				and visualize data with the aim of improving decision-making. In
-				addition to my development role, a significant part of my
-				responsibilities involved studying our clients' business processes to
-				create BPDs and plan database infrastructure to make sure we would
-				implement the most appropriate technologies to our solutions. Moreover,
-				I was actively involved in educating new recruits about our technology
+				Our devlopment's core laid in understanding the specific processes of
+				our organization and engineering systems that enhance and automate their
+				workflows to increase operational efficiency. We specialized in creating
+				solutions that strategically collect, analyze, and visualize data to
+				provide insights and improve decision-making.
+			</span>
+			<span class="mt-4 block font-light"
+				>Clearly defined user stories, designed Business Process Diagrams and
+				planed database infrastructure
+			</span>
+			<span class="mt-4 block font-light"
+				>I was actively involved in educating new recruits about our technology
 				stack, ensuring they are well-equipped and knowledgeable for their roles
 				after my departure.
 			</span>
@@ -128,87 +139,53 @@
 			{/if}
 		</button>
 	</div>
-	<div class="block-stone-300 relative font-mono font-bold">
-		<span class="code-line-pink text-white">IDF</span>&nbsp; Started as a
-		network manager and after 5 months became Team Leader of the IT department.
-		The department consisted of up to 20 soldiers with different tiers and
-		responsibilities. Our role consisted of administrating, maintaining and
-		improving on-premises network infrastructure and over 400 work stations and
-		servers as well as handling requests from organization members and handling
-		logtistics and stock with suppliers and vendors.
-		{#if showMore}
-			<span class="font-light">
-				I led a team of mobile and web developers inthe Israeli Defence Forces.
-				Our work involved understanding the specific processes of our
-				organization and engineering systems that enhance and automate their
-				workflows. We specialized in creating solutions that collect, analyze,
-				and visualize data with the aim of improving decision-making. In
-				addition to my development role, a significant part of my
-				responsibilities involves studying our clients' business processes to
-				create BPDs and plan database infrastructure to make sure we implement
-				the most appropriate technologies to our solutions. Moreover, I was
-				actively involved in educating new recruits about our technology stack,
-				ensuring they are well-equipped and knowledgeable for their roles after
-				my departure.
-			</span>
-		{/if}
-		<button
-			class="absolute bottom-5 right-5 flex items-center justify-center rounded-full bg-white p-1 text-xs text-black hover:text-blue-700"
-			onclick={toggleShowMore}
+	<div class="block-stone-300 relative p-5 md:p-10">
+		<span class="code-line-pink text-white">IDF</span>&nbsp;&nbsp;&nbsp;<span
+			class="font-bold">Supervised a department up to 20 soldiers</span
 		>
-			{#if showMore}
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="lucide lucide-chevron-up"
-				>
-					<path d="m18 15-6-6-6 6" />
-				</svg>
-			{:else}
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="lucide lucide-chevron-down"
-				>
-					<path d="m6 9 6 6 6-6" />
-				</svg>
-			{/if}
-		</button>
+		with different tiers and responsibilities. Our role consisted of administrating,
+		maintaining and improving on-premises network infrastructure and over 400 work
+		stations and servers as well as handling requests from organization members and
+		handling logistics and stock with suppliers and vendors.
+
+		<span class="mt-4 block font-light">
+			Directed a comprehensive overhaul of network systems, significantly
+			improving stability and performance across more than 400 workstations and
+			servers.
+		</span>
+	</div>
+	<div class="block-stone-300 relative p-5 md:p-10">
+		<span class="code-line-pink text-white">IDF</span>&nbsp;&nbsp;&nbsp;Started
+		as a network adminisrator and got
+		<span class="font-bold">promoted to Team Leader within six months </span>
+		due to leadership, technological proficiency and creative innitiatve.
+
+		<span class="mt-4 block font-light"
+			>Created documentation, automated departmental processes and developed
+			SharePoint pages to ease interactions and information transfer within and
+			between the branches.</span
+		>
 	</div>
 </section>
-<section>
+<section class="section">
 	<h3 class="text-lg font-black underline underline-offset-8">Details</h3>
 	<h3 class="code-line">I flex:</h3>
-	<ul>
+	<ul class="ul">
 		<li>I use vim btw</li>
 		<li>I run linux btw</li>
 	</ul>
 </section>
-<section>
+<section class="section">
 	<h3 class="code-line">languages I'm proficient in:</h3>
-	<ul>
+	<ul class="ul">
 		<li>Typescript</li>
 		<li>Rust</li>
 		<li>Go</li>
 	</ul>
 </section>
-<section>
+<section class="section">
 	<h3 class="code-line">technologies:</h3>
-	<ul>
+	<ul class="ul">
 		<li>Agile Project Management</li>
 		<li>D3</li>
 		<li>Three.js</li>
@@ -233,11 +210,11 @@
 </section>
 
 <style>
-	section {
-		@apply flex flex-col gap-4;
+	.section {
+		@apply m-auto mb-7 flex w-full max-w-[900px] flex-col gap-4;
 	}
 
-	ul {
+	.ul {
 		@apply list-disc pl-8;
 	}
 </style>
