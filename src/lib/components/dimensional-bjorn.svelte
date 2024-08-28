e6abbafc453e8b8b142fa96ba3b316a13caf58c0
Mayer Starkman <mstarkcode@gmail.com>
2024-08-28 20:04:12 +0300

    fix: return h-full on loader div

--- src/lib/components/dimensional-bjorn.svelte
+++ src/lib/components/dimensional-bjorn.svelte
@@ -237,7 +237,10 @@
 </script>
 
 {#if settings.useLoadingAnimation}
-	<div class="flex w-full items-center justify-center {!isLoading && 'hidden'}">
+	<div
+		class="flex h-full w-full items-center justify-center {!isLoading &&
+			'hidden'}"
+	>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="24"
