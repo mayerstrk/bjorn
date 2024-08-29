4cb3422c7fba8916491089015543b479fd7dd07f
Mayer Starkman <mstarkcode@gmail.com>
2024-08-29 16:46:12 +0300

    perf: try to optimize config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		enhancedImages(),
-		visualizer({ emitFile: true, filename: 'stats.html' })
-	],
+	plugins: [sveltekit(), enhancedImages()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
 		host: '127.0.0.1'
+	},
+	optimizeDeps: {
+		include: ['three']
 	}
 });
