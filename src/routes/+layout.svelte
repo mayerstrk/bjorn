a40648f84b08f8482f315b937357335e2e6a7aa6
Mayer Starkman <mstarkcode@gmail.com>
2024-11-02 22:29:14 +0200

    style(layout): reduce top margin for footer on small screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 	>
 		<slot />
 	</div>
-	<footer class="relative mt-96 flex w-full flex-col text-xs md:mt-0">
+	<footer class="relative mt-40 flex w-full flex-col text-xs md:mt-0">
 		<p class="mx-auto pb-3 text-xs opacity-50 md:mx-0">
 			&copy; 2024 Mayer Starkman. All rights reserved.
 		</p>
